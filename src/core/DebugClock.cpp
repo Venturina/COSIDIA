6a36314a5095cb5e1aa8443c6f4fa845cae14749
Christina Obermaier <christina.obermaier@thi.de>
2019-08-09 14:50:23 +0200

    DebugClock: adds Clock for debugging without timing constraints: runs as fast as possible

---
+++ src/core/DebugClock.cpp
@@ -0,0 +1,29 @@
+#include "core/DebugClock.hpp"
+
+namespace paresis
+{
+
+std::chrono::time_point<std::chrono::steady_clock> DebugClock::getRealTimeForCurrentSimTime()
+{
+    return std::chrono::steady_clock::now();
+}
+
+std::chrono::nanoseconds DebugClock::getSimTimeNow()
+{
+    return mSimTime;
+}
+
+std::chrono::nanoseconds DebugClock::getDurationUntil(std::chrono::nanoseconds until)
+{
+    auto currentTime = mSimTime;
+    updateSimTime(until);
+    return until - currentTime;
+}
+
+std::chrono::time_point<std::chrono::steady_clock> DebugClock::getTimePointforSimTime(std::chrono::nanoseconds until)
+{
+    return std::chrono::steady_clock::now() + getDurationUntil(until);
+}
+
+
+}
---
+++ src/core/DebugClock.hpp
@@ -0,0 +1,26 @@
+#ifndef _DEBUG_CLOCK_HPP_SDNFO
+#define _DEBUG_CLOCK_HPP_SDNFO
+
+#include "core/SteadyClock.hpp"
+
+namespace paresis
+{
+
+class DebugClock : public SteadyClock
+{
+public:
+    DebugClock(float simSpeed) : SteadyClock(simSpeed) {};
+
+    virtual std::chrono::time_point<std::chrono::steady_clock> getRealTimeForCurrentSimTime() override;
+    virtual std::chrono::nanoseconds getSimTimeNow() override;
+    virtual std::chrono::nanoseconds getDurationUntil(std::chrono::nanoseconds) override;
+    virtual std::chrono::time_point<std::chrono::steady_clock> getTimePointforSimTime(std::chrono::nanoseconds) override;
+
+
+private:
+
+};
+
+} // namespace paresis
+
+#endif /* _DEBUG_CLOCK_HPP_SDNFO */
