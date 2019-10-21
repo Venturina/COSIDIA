2b25c06a1339992ede9ae2e864e5624bfa20454f
Christina Obermaier <christina.obermaier@thi.de>
2019-10-21 09:58:00 +0200

    SimulationManager: create unique Objects

--- src/objects/SimulationManager.cpp
+++ src/objects/SimulationManager.cpp
@@ -25,7 +25,7 @@ void SimulationManager::startAndScheduleObject(std::shared_ptr<BaseObject> obj)
 {
     obj->setParent(mObjectId);
     obj->setObjectId(getCoreP()->getNextObjectId());
-    getCoreP()->addObject(obj);
+    getCoreP()->addUniqueObject(obj);
 
     auto action = std::make_shared<Action>(SteadyClock::duration{0}, Action::Kind::INIT, SteadyClock::duration{0}, obj->getObjectId());
     getCoreP()->scheduleAction(std::move(action));
