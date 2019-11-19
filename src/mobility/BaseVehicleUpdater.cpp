10e8d733756bfe314458b3397b02a8fab05e7f93
Christina Obermaier <christina.obermaier@thi.de>
2019-11-19 09:51:42 +0100

    fix: add missing files

---
+++ src/mobility/BaseVehicleUpdater.cpp
@@ -0,0 +1,20 @@
+#include "mobility/BaseVehicleUpdater.hpp"
+#include "traci/LiteAPI.h"
+
+namespace paresis
+{
+
+BaseVehicleUpdater::BaseVehicleUpdater(traci::LiteAPI* api) : mLiteAPI(api)
+{
+}
+
+BaseVehicleUpdater::~BaseVehicleUpdater()
+{
+}
+
+VehicleDataUpdate BaseVehicleUpdater::updateAllVehicles()
+{
+    
+}
+
+}
---
+++ src/mobility/BaseVehicleUpdater.hpp
@@ -0,0 +1,40 @@
+#ifndef BASE_VEHICLE_UPDATER_HPP_SHON
+#define BASE_VEHICLE_UPDATER_HPP_SHON
+
+#include "mobility/MobilityManager.hpp"
+#include "core/Action.hpp"
+
+namespace traci
+{
+    class LiteAPI;
+}
+
+namespace paresis
+{
+
+struct VehicleUpdate
+{
+    int externalId; // eg. SumoId
+    int speed; // example for for all respective vehicle informations
+};
+
+class VehicleDataUpdate : public ActionData
+{
+    
+};
+
+class BaseVehicleUpdater
+{
+
+public:
+    BaseVehicleUpdater(traci::LiteAPI*);
+    ~BaseVehicleUpdater();
+
+    virtual VehicleDataUpdate updateAllVehicles();
+private:
+    traci::LiteAPI* mLiteAPI;
+};
+
+
+} // ns paresis
+#endif /* BASE_VEHICLE_UPDATER_HPP_SHON */
