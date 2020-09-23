a544fc22d6c1f9b23c8a0aa48ace1dabc8161a33
christina <christina@localhost.localdomain>
2020-09-23 11:10:35 +0200

    documentation: Action

--- src/core/Action.hpp
+++ src/core/Action.hpp
@@ -19,7 +19,7 @@ typedef int ActionId;
 
 /** A action describes
  *  a specific action in the paresis system.
- * It contains a start time point, a duration and something to execute
+ *  It contains a start time point, a duration and something to execute
  */
 
 class Action
@@ -31,32 +31,105 @@ public:
         END
     };
 
-    //Action(uint32_t duration) : mDuration(duration) {}
+    /**
+     * Creates an Action for a specific object
+     *
+     * @param duration Time span a action takes to be calculated
+     * @param k Kind of Action (Init, Start, End)
+     * @param start Time when the action should be started
+     * @param id Object which is the receiver of the Action
+     */
     Action(SteadyClock::duration duration, Kind k, SteadyClock::duration start, int id);
+
+    /**
+     * Creates an Action for a list of object
+     *
+     * @param duration Time span a action takes to be calculated
+     * @param k Kind of Action (Init, Start, End)
+     * @param start Time when the action should be started
+     * @param ids Objects which receive the Action
+     */
     Action(SteadyClock::duration duration, Kind k, SteadyClock::duration start, std::list<int> ids);
+
+    /**
+     * Get start time of an Action
+     */
     SteadyClock::duration getStartTime() const { return mStartTime; }
+
+    /**
+     * Get duration of an Action
+     */
     SteadyClock::duration getDuration() const { return mDuration; }
 
+    /**
+     * Get attached ActionData, nullptr if no ActionData is attached
+     */
     std::shared_ptr<ActionData> getActionData() { return mActionData; }
 
+    /**
+     * Set start time of Action
+     * @param start The start Time
+     */
     void setStartTime(SteadyClock::duration start);
+
+    /**
+     * Attach ActionData to Action
+     * @param ActionData to attach, overriding of existing ActionData is not allowed
+     */
     void setActionData(std::shared_ptr<ActionData>);
 
+    /**
+     * Get Kind of Action
+     */
     Kind getKind() { return mKind; }
+
+    /**
+     * Get all affected Objects
+     *
+     * @return Pointer to List of all Affected Objects
+     */
     std::list<int>* getAffected() { return &mAffectedObjects; };
 
     /**
-     * Set type of Action: for more specific processing
+     * Set type string of Action: for more specific processing
      */
     void setType(std::string type) { mType = type; }
+
+    /**
+     * Get type string of Action
+     */
     std::string getType() { return mType; }
 
+    /**
+     * Set ID of Action
+     *
+     * @param Id of Action, can be overridden
+     */
     void setActionId(int id) { mActionId = id; }
+
+    /**
+     * Get id of Action
+     */
     int getActionId() { return mActionId; }
 
+    /**
+     * Required for Actions with Kind=END
+     * Has to correspond to the ActionId of Start Action
+     */
     void setBeginId(int id);
+
+    /**
+     * Returns corresponding Start Action ID
+     *
+     * @return 0 for Start and Init Actions
+     */
     int getBeginId() { return mBeginActionId; };
 
+    /**
+     * Adds additional affected objects by ID
+     *
+     * @param id of the affected Object
+     */
     void addAffected(int id);
 
 
@@ -90,7 +163,13 @@ protected:
 private:
     Kind mKind;
 
+
     int mActionId = 0;
+
+    /**
+     * Required for Actions with Kind=END
+     * Has to contain the ID of the corresponding START Action
+     */
     int mBeginActionId = 0;
 
     std::string mType = "";
