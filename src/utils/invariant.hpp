1d53d8090658cd0ce8b3e17270fab177478b1114
Raphael Riebl <raphael@riebl.net>
2021-01-18 14:06:32 +0100

    fix COSIDIA_SAFE typos

--- src/utils/invariant.hpp
+++ src/utils/invariant.hpp
@@ -1,7 +1,7 @@
 #ifndef _INVARIANT_HPP_SNDVO
 #define _INVARIANT_HPP_SNDVO
 
-#ifdef CONSIDIA_SAFE
+#ifdef COSIDIA_SAFE
 #define invariant_(condition, msg) \
     if (!condition) { throw std::runtime_error(msg); }
 #else
