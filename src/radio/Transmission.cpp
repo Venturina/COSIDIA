44146ba960bf6093a581767c60898717b5e6a942
Christina Obermaier <christina.obermaier@yahoo.de>
2021-01-22 22:50:52 +0100

    Radio: adds transmission

--- src/radio/Transmission.cpp
+++ src/radio/Transmission.cpp
@@ -1,7 +1,30 @@
 #include <radio/Transmission.hpp>
+#include <utils/enforce.hpp>
 
-
-const CartesianPoint& Transmission::getEmitterPosition()
+namespace cosidia
 {
-    return mEmitterPosition;
+
+void Transmission::setupReceiverContext()
+{
+    enforce(onCoreThread(), "Transmission: setupReceiverContext() on wrong thread");
+    enforce(mIsSetupDone == false, "Tranmssion: duplicate setup");
+
+    for(auto& receiver : mPossibleReceivers) {
+        receiver.second.receiverContext = receiver.second.mobility.lock()->getContext();
+    }
+
+    mIsSetupDone == true;
 }
+
+void Transmission::addPossibleReceiver(ObjectId id, std::shared_ptr<VehicleObject> vehicle)
+{
+    Receiver r;
+    enforce(id.valid(), "Transmission: invalid Receiver Id")
+    enforce(vehicle, "Transmission: vehicle Object not valid")
+    r.lowestLayer = id;
+    r.mobility = vehicle;
+
+    mPossibleReceivers[id.raw()] = r;
+}
+
+} //ns cosidia
--- src/radio/Transmission.hpp
+++ src/radio/Transmission.hpp
@@ -1,20 +1,50 @@
 #ifndef TRANSMISSION_HPP_LNSFO
 #define TRANSMISSION_HPP_LNSFO
 
-#include <boost/geometry/geometries/point_xy.hpp>
-#include "boost/date_time/posix_time/posix_time_types.hpp"
+#include "actionData/ActionData.hpp"
+#include "objects/VehicleObject.hpp"
+#include "objects/ObjectId.hpp"
 
-typedef boost::geometry::model::d2::point_xy<double> CartesianPoint;
+#include "vanetza/access/data_request.hpp"
+#include "vanetza/net/chunk_packet.hpp"
 
-class Transmission {
+#include <list>
+
+namespace cosidia {
+class Transmission : public ActionData {
+    /**
+     * A receiver consists of the lowest layer after the radio and a mobility module for fetching the receiver vehicle's dynamics;
+     **/
+    struct Receiver
+    {
+        Receiver() : lowestLayer(-1) {};
+
+        ObjectId lowestLayer;  //TODO has to change to the current lower layer
+        std::weak_ptr<VehicleObject> mobility;
+        std::shared_ptr<const VehicleObjectContext> receiverContext;
+
+        bool reachable = true;
+    };
+    
 public:
-    const CartesianPoint& getEmitterPosition();
-    const boost::posix_time::ptime emittingTime;
+    Transmission(const vanetza::access::DataRequest request, std::unique_ptr<vanetza::ChunkPacket> packet):
+        mEmitter(-1), mRequest(request), mPacket(std::move(packet)), ActionData("transmission") {};
+
+    void setupReceiverContext();
+    void addPossibleReceiver(ObjectId, std::shared_ptr<VehicleObject>);
 
 private:
-    CartesianPoint mEmitterPosition;
-    boost::posix_time::ptime mEmittingTime;
+    ObjectId mEmitter;
+    std::weak_ptr<VehicleObject> mobility;
+    std::shared_ptr<const VehicleObjectContext> mEmitterContext;
 
+    //TODO ObjectId instead of int!
+    std::map<int, Receiver> mPossibleReceivers;
+
+    const vanetza::access::DataRequest& mRequest;
+    std::shared_ptr<vanetza::ChunkPacket> mPacket;
+
+    bool mIsSetupDone = false;
 };
-
+} // ns cosidia
 #endif /* TRANSMISSION_HPP_LNSFO */
